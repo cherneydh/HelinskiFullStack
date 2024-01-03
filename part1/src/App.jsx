d5e12adba242b48e90da2579341af0238b62df57
Daniel Cherney <dcherney@acm.org>
2024-01-02 20:54:25 -0800

    Finished Part1a exercises 1.1 & 1.2

---
+++ part1/src/App.jsx
@@ -0,0 +1,47 @@
+const Header = (props) => {
+  return (
+    <h1>{props.course_name}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+    <Part name={props.part1} number={props.exercises1} />
+    <Part name={props.part2} number={props.exercises2} />
+    <Part name={props.part3} number={props.exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.number}</p>
+  )
+}
+
+const Footer = (props) => {
+  return (
+    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course_name={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
+      <Footer exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
+  )
+}
+
+export default App
